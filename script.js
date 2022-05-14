effaf1e2551513558d242c8846a31072159efa94
rajpranz <rajeshwarrprasad@yahoo.com>
2022-05-14 20:38:51 +1200

    Validate user input

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 console.log("Welcome to Rock Paper Scissors");
 console.log("Best of five wins");
-console.log("Whats your move:");
+var userMove = "paper";
+//userMove = getUserInput();
+
 console.log(computerMove());
 
 function computerMove() {
     let moves = ["rock", "paper", "scissors"];
     return moves[Math.floor(Math.random() * 3)];
 }
+
+function getUserInput() {
+    let userMove = prompt("Whats your move? ");
+    userMove =  userMove.trim().toLowerCase();
+    if(userMove == "rock" || userMove == "paper" || userMove == "scissors"){
+        console.log("Valid move");
+        return userMove;
+    }else{
+        console.log("Invalid move. Try again");
+        return getUserInput();
+    }   
+}
+
 
