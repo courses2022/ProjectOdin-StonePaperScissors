1399afd9ad97e452930ff624d560e7d08a5c7ff2
rajpranz <rajeshwarrprasad@yahoo.com>
2022-05-14 16:32:24 +1200

    Generate Random computer move

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Welcome to Rock Paper Scissors");
+console.log("Best of five wins");
+console.log("Whats your move:");
+console.log(computerMove());
+
+function computerMove() {
+    let moves = ["rock", "paper", "scissors"];
+    return moves[Math.floor(Math.random() * 3)];
+}
+
