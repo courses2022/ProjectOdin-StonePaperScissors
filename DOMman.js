3b129ee6cb24500c1046e14e46e5bc02f0216107
rajpranz <rajeshwarrprasad@yahoo.com>
2022-05-17 11:15:19 +1200

    Add event listeners

--- DOMman.js
+++ DOMman.js
@@ -17,3 +17,18 @@ content.setAttribute('style', 'border: 3px solid black; background-color:pink');
 content.innerHTML="<h2>Im in a div</h2><p>ME TOO!</p>";
 container.appendChild(content);
 
+const btn = document.querySelector("#btn");
+btn.onclick = () => alert("Hello World 2");
+
+const btn3 = document.querySelector('#btn3');
+btn3.addEventListener('click', function (e) {
+    console.log(e.target);
+    e.target.style.background = 'blue';
+  });
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+button.addEventListener('click', () => {
+    alert(button.id);
+  });
+});
